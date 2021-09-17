12dd3a1d20ff715bfea81d85fe0fe4595ad9f9d6
Candra Fried <candracodes@gmail.com>
2021-09-17 15:28:00 -0500

    Finally got a successful fetch/response for a 5 day forecast. Unfortunately it is displaying 10 day, so I need to figure out how to remove the extra days, and to convert the days of the week from id numbers to actual days of the week.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-// Define HTML elements as variables to reference later:
-var storedCityLiEl = document.getElementById("city-result-list-item");
-var todaysForecastContainerEl = document.getElementById("todays-forecast-container");
-var weatherCardContainerEl = document.getElementById("weather-card-container");
-var weatherCardEl = document.getElementById("weather-card");
+// Define HTML elements as variables to reference later (possibly)
+// var storedCityLiEl = document.getElementById("city-result-list-item");
+// var todaysForecastContainerEl = document.getElementById("todays-forecast-container");
+// var weatherCardContainerEl = document.getElementById("weather-card-container");
+// var weatherCardEl = document.getElementById("weather-card");
 
 /* ================================================================================== */
 
@@ -50,7 +50,6 @@ var getRequestedCity = function (city) {
   fetch(queryURL)
     .then(function (response) {
       if (response.ok) {
-        console.log(response);
         response.json().then(function (data) {
           console.log(data);
 
@@ -67,14 +66,16 @@ var getRequestedCity = function (city) {
           // Convert Kelvin to Fahrenheit:
           // (K − 273.15) × 9/5 + 32 = °F. (where K is the value the computer initially spews)
 
-          // DISPLAY TEMPERATURE
+          // DISPLAY TEMPERATURE (NOTE: this could be achieved by simply referring to the metric parameter, but I want to hold on to this formula in case I need it in the future)
           $("#todays-forecast-ul").append("<li>Temp: " + Math.floor(((data.main.temp - 273.15) * 9 / 5 + 32)) + "° </li>");
           // DISPLAY WIND SPEED
           $("#todays-forecast-ul").append("<li>Wind: " + data.wind.speed + " mph</li>");
           // DISPLAY HUMIDITY
           $("#todays-forecast-ul").append("<li>Humidity: " + data.main.humidity + "%</li>");
 
-          // TODO: UV Index is not a key in the queryURL parameter, so I have to use another endpoint to produce this
+          // TODO: UV Index is not a key in the queryURL parameter, so I have to use another endpoint to produce this. In the mean time...
+          // DISPLAY UV INDEX
+          $("#todays-forecast-ul").append("<li>UV Index: Unknown</li>");
 
           // TODO: Stop this function from concatenating additional UL elements after the search button is clicked. It should only display one result
 
@@ -91,8 +92,59 @@ var getRequestedCity = function (city) {
 };
 
 // 2. getFiveDayForecast() | Create a function that displays the 5 day forecase of the requested city
-function getFiveDayForecast() {
-  console.log("getFiveDayForecast function triggered");
+function getFiveDayForecast(city) {
+  // ******* Starting over completely from scratch. The vanila Javascript just wasn't doing the trick for me
+
+  // Collect the user entered city value
+  city = $(".search-input").val();
+
+  // Defining the first portion of the openweathermap API url 
+  var queryFiveDayURL = "https://api.openweathermap.org/data/2.5/forecast";
+
+  // "Fetching" the data by constructing with ajax
+  $.ajax({
+    url: queryFiveDayURL,
+    dataType: "json",
+    type: "GET",
+    data: {
+      // parameters and documentation found here: https://openweathermap.org/forecast5
+      q: city,
+      appid: openWeatherAPIKey,
+      units: "imperial",
+      cnt: "10"
+    },
+    success: function (data) {
+      // TEST: Display all data to examine the keys and values
+      console.log('Received data:', data)
+      // TEST: View the fully concatenated string in the console
+      console.log("The URL string = " + queryFiveDayURL +"?q=" + city +"&appid=" + openWeatherAPIKey);
+      // Create empty string to use in various was for UI elements
+      var constructUI = "";
+      // Run a loop that construct the full blue weather card
+      $.each(data.list, function (index, val) {
+        // Open the beginning of column, and weather card
+        constructUI += "<div class='card weather-card'><div class='card-body'><h5 class='card-title'>" + index + "</h5><ul>"
+        // ADD Icon
+        constructUI += "<li><img src='https://openweathermap.org/img/w/" + val.weather[0].icon + ".png'></li>";
+
+        // ADD Temp + Round the decimal
+        constructUI += "<li>Temp: " + Math.floor(val.main.temp) + "°F</li>";
+
+        // ADD Wind
+        constructUI += "<li>Wind: " + val.wind.speed + " mph</li>";
+
+        // ADD Humidity
+        constructUI += "<li>Humidity: " + val.main.humidity + " %</li>";
+
+        // CLOSE Entire UI Element
+        constructUI += "</ul></div></div>";
+
+      });
+      // Append the UI elements to the parent HTML element
+      $(".weather-card-container").html(constructUI);
+    }
+  });
+
 }
 
 // 3. storePreviousCities() | Create a function that stores previously searched city results, makes it an <a>, and, if clicked, replaces the today's forecast and 5 day forecast with chosen city
@@ -107,7 +159,7 @@ function clearPreviousCities() {
 
 // 5. Add event listener for search button
 $("#search-btn").on("click", function () {
-  console.log("Search Button triggered");
+
   getRequestedCity();
   getFiveDayForecast();
 
@@ -118,9 +170,12 @@ $("#clear-btn").on("click", function () {
   // CLEAR THE SEARCH INPUT FIELD
   $(".search-input").val("");
   // CLEAR TODAY'S FORECAST
-  document.getElementById('todays-forecast-ul').innerHTML='';
+  document.getElementById('todays-forecast-ul').innerHTML = '';
   //CLEAR THE CITY <H2>
-  document.getElementById('city-result-h2').innerHTML='';
+  document.getElementById('city-result-h2').innerHTML = '';
+  // CLEAR BLUE 5DAY WEATHER CARDS
+  // document.getElementById('#clearBlueBoxes').empty();
+  $( "ul" ).empty();
   // CLEARING LOCAL STORAGE FOR FUTURE SAVED RESULTS
   localStorage.clear();
 });
