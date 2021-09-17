21efb8d6964a08cec6de4121f052aed7d54bb3e7
Candra Fried <candracodes@gmail.com>
2021-09-17 18:58:05 -0500

    Well... something is happening in the sidebar. There are results, but they're wonky. I think I might be throwing the towel in for now. If I ever hope to accomplish anything with this group project, then I have to abandon this homework assignment for the time being. I will endeavor to resubmit when time permits.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
-// Define HTML elements as variables to reference later (possibly)
-// var storedCityLiEl = document.getElementById("city-result-list-item");
-// var todaysForecastContainerEl = document.getElementById("todays-forecast-container");
-// var weatherCardContainerEl = document.getElementById("weather-card-container");
-// var weatherCardEl = document.getElementById("weather-card");
+/* ========================================================================================================== */
+/* 
+NOTE TO REVIEWER:
+THINGS THAT WORK:
+1. Proper display of Today's Forecast (parameters include: date, icon, temp, wind, humidity)
+2. Proper display of 5 Day Forecast
+3. Cities saved to local storage in side bar, but they only display after a user enters a second search
+*/
+/* ========================================================================================================== */
+
+// TODO: 1. Figre out a way to display locally stored results immediately after a user hits "Search", and NOT JUST after a user clicks Search for the second time
+// TODO: 2. Figure out how to stop "Today's Forecast" from duplicating when a user enters a new city. It should obliterate the previous "Today's Forecast"
+// TODO: 3. Figure out how to cross-reference endpoints so I can display the UV Index in Today's Forecast
+// TODO: 4. Once UV Index is working, add conditional statements that will display color options for classes like: favorable, moderate, and severe
+// TODO: 5. Figure out a way to prevent the "Error: Bad Request" when a user clicks on a city result
+
+/* ================================================================================== */
+
+// These elements are necessary for local storage items in the sidebar
+var currentCity = "";
+var lastCity = "";
 
 /* ================================================================================== */
 
@@ -30,6 +46,7 @@ displayCurrentDate();
 // 1. getRequestedCity() | Create a function that takes value of user entered city, and utilizes previously city variable
 var getRequestedCity = function (city) {
   city = $(".search-input").val();
+  currentCity= $('#search-input').val();
   console.log("City value = " + city);
 
   // HOW TO CONSTRUCT A QUERY URL IN OPENWEATHER:
@@ -52,6 +69,8 @@ var getRequestedCity = function (city) {
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data);
+          // attempting to save to local storage.
+          storePreviousCities(city);
 
           // Display the City <H2>
           $("#city-result-h2").text(data.name);
@@ -158,24 +177,78 @@ function getFiveDayForecast(cityData) {
 }
 
 // 3. storePreviousCities() | Create a function that stores previously searched city results, makes it an <a>, and, if clicked, replaces the today's forecast and 5 day forecast with chosen city
-function storePreviousCities() {
+function storePreviousCities(newCity) {
   console.log("storePreviousCities function triggered");
+  // Assuming it's a user's first time here, then this "false" is a true statement and I want to default to this
+  var cityExists = false;
+
+  // Check to see if City boolean is true, i.e. "Is there anything in storage"
+  for (let i = 0; i < localStorage.length; i++) {
+      if (localStorage["cities" + i] === newCity) {
+          cityExists = true;
+          break;
+      }
+  }
+  // Add to local storage if the city is new
+  if (cityExists === false) {
+      localStorage.setItem('cities' + localStorage.length, newCity);
+  }
 }
 
-// 4. clearPreviousCities() | Create a function that clears the results of the previously searched cities
-function clearPreviousCities() {
-  console.log("clearPreviousCities function triggered");
+// 4. showPreviousCities() | Create a function that actually constructs the city results in sidebar and makes them clickable
+function showPreviousCities() {
+  console.log("showPreviousCities function triggered");
+
+  if (localStorage.length===0){
+    if (lastCity){
+        $('#search-input').attr("value", lastCity);
+    } else {
+        $('#search-input').attr("value", "Dallas");
+    }
+} else {
+    // Build key of last city written to localStorage
+    let lastCityKey="cities"+(localStorage.length-1);
+    lastCity=localStorage.getItem(lastCityKey);
+    // Set search input to last city searched
+    $('#search-input').attr("value", lastCity);
+    // Append stored cities to page
+    for (let i = 0; i < localStorage.length; i++) {
+        let city = localStorage.getItem("cities" + i);
+        let cityEl;
+        // Set to lastCity if currentCity not set
+        if (currentCity===""){
+            currentCity=lastCity;
+        }
+        // Set button class to active for currentCity
+        if (city === currentCity) {
+            cityEl = `<li class="city-result-list-item"><a onclick="getRequestedCity()">${city}</a></li>`;
+        } else {
+            cityEl = `<li class="city-result-list-item"><a onclick="getRequestedCity()">${city}</a></li>`;
+        } 
+        // Append city to page
+        $('#city-result-ul').prepend(cityEl);
+    }
+}
 }
 
 // 5. Add event listener for search button
 $("#search-btn").on("click", function () {
 
   getRequestedCity();
-  // getFiveDayForecast();
+  currentCity = $('#search-input').val();
+  showPreviousCities();
+
+});
+// 6. Add event listener to make historical data clickable
+$("#city-result-ul").on("click", function (event) {
+  // TODO: not exactly working how I want, but maybe I can fix this and resubmit homework
+  event.preventDefault();
+  $('#search-input').val(event.target.textContent);
+  currentCity = $('#search-input').val();
 
 });
 
-// 6. Add event listener for clear button
+// 7. Add event listener for clear button
 $("#clear-btn").on("click", function () {
   // CLEAR THE SEARCH INPUT FIELD
   $(".search-input").val("");
@@ -183,9 +256,12 @@ $("#clear-btn").on("click", function () {
   document.getElementById('todays-forecast-ul').innerHTML = '';
   //CLEAR THE CITY <H2>
   document.getElementById('city-result-h2').innerHTML = '';
-  // CLEAR BLUE 5DAY WEATHER CARDS
-  // document.getElementById('#clearBlueBoxes').empty();
-  $( "ul" ).empty();
-  // CLEARING LOCAL STORAGE FOR FUTURE SAVED RESULTS
+  // CLEAR SIDEBAR RESULTS
+  $( "#city-result-ul" ).empty();
+  // TODO: FIGURE OUT A WAY TO CLEAR OUT THE 5 DAY FORECAST
+  // CLEARING LOCAL STORAGE
   localStorage.clear();
 });
+
+// 8. Call the function to display previously searched cities
+showPreviousCities();
